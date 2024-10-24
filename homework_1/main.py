22dc0b44781682d15f974e33b0eed644b3acf4c2
S-H-A-K-H-Z-O-D <m.shakhzod2002@gmail.com>
2024-10-24 16:45:41 +0500

    solved 20 problems

---
+++ homework_1/main.py
@@ -0,0 +1,200 @@
+# ----------------------Task 1----------------------
+# a = float(input("Enter square's side length: "))
+
+# P = 4 * a
+
+# print(f"Perimetr: {P}")
+
+# ----------------------Task 2----------------------
+# a = float(input("Enter square's side length: "))
+
+# S = a ** 2
+
+# print(f"Area: {S}")
+
+# ----------------------Task 3----------------------
+# a = float(input("Enter quadrangle's side length: "))
+# b = float(input("Enter quadrangle's side length: "))
+
+# P = 2 * (a + b)
+# S = a * b
+
+# print(f"Perimetr: {P}")
+# print(f"Area: {S}")
+
+# ----------------------Task 4----------------------
+# d = float(input("Enter diametr's length: "))
+# pi = 3.14
+
+# L = pi * d
+
+# print(f"Circle round: {L:.2f}")
+
+# ----------------------Task 5----------------------
+# a = float(input("Enter cube's side length: "))
+
+# P = 6 * a ** 2
+# V = a ** 3
+
+# print(P)
+# print(V)
+
+# ----------------------Task 6----------------------
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+# c = float(input("Enter c: "))
+
+# V = (a * b * c)
+# S = 2 * (a * b + a * c + b * c)
+
+# print(f"Size: {V}")
+# print(f"Area: {S}")
+
+# ----------------------Task 7----------------------
+# r = float(input("Enter radius: "))
+# pi = 3.14
+
+# L = 2* pi * r
+# S = pi * r ** 2
+
+# print(L)
+# print(S)
+
+# ----------------------Task 8----------------------
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+
+# l = (a + b) / 2
+
+# print(l)
+
+# ----------------------Task 9----------------------
+# import math
+
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+
+# l = math.sqrt(a * b)
+
+# print(l)
+
+# ----------------------Task 10----------------------
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+
+# l = a + b
+# m = a * b
+# n = a ** 2 + b ** 2
+
+# print(l)
+# print(m)
+# print(n)
+
+# ----------------------Task 11----------------------
+# import math
+
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+
+# l = a + b
+# m = a * b
+# n = abs(a - b)
+
+# print(l)
+# print(m)
+# print(n)
+
+# ----------------------Task 12----------------------
+# import math
+
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+# c = float(input("Enter c: "))
+
+# P = a + b + c
+# c = math.sqrt(a ** 2 + b ** 2)
+
+# print(P)
+# print(c)
+
+# ----------------------Task 13----------------------
+# import math
+
+# r1 = float(input("Enter r1: "))
+# r2 = float(input("Enter r2: "))
+
+# S1 = math.pi * r1 ** 2
+# S2 = math.pi * r2 ** 2
+# S3 = S1 - S2
+
+# print(S1)
+# print(S2)
+# print(S3)
+
+# ----------------------Task 14----------------------
+# r = float(input("Enter radius: "))
+# pi = 3.14
+
+# L = 2* pi * r
+# S = pi * r ** 2
+
+# print(L)
+# print(S)
+
+# ----------------------Task 15----------------------
+# r = float(input("Enter radius: "))
+# pi = 3.14
+
+# L = 2* pi * r
+# S = pi * r ** 2
+
+# print(L)
+# print(S)
+
+# ----------------------Task 16----------------------
+# import math
+
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+
+# n = abs(a - b)
+
+# print(n)
+
+# ----------------------Task 17----------------------
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+# c = float(input("Enter c: "))
+
+# ac = abs(a - c)
+# bc = abs(b - c)
+# l = ac + bc
+
+# print(ac)
+# print(bc)
+# print(l)
+
+# ----------------------Task 18----------------------
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+# c = float(input("Enter c: "))
+
+# ac = abs(a - c)
+# bc = abs(b - c)
+# l = ac + bc
+
+# print(ac)
+# print(bc)
+# print(l)
+
+# ----------------------Task 20----------------------
+# import math
+
+# x1 = float(input("Enter x1: "))
+# x2 = float(input("Enter x2: "))
+# y1 = float(input("Enter y1: "))
+# y2 = float(input("Enter y2: "))
+
+# l = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+# print(l)
